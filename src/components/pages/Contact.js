dc93b58cd48b7d37d58e9967989034d8eadcb83e
Zoe Rorvig <zrorvig@gmail.com>
2023-02-04 19:17:12 -0500

    added functionality to contacts

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,37 +1,61 @@
 import React, { useState } from 'react';
 
 export default function Contact() {
-  const [fullName, setName] = useState('');
+    const [fullName, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
 
-    return name === 'fullName' ? setName(value) : "Required";
-  };
+        if (name === "fullName") {
+            setName(value);
+        } else if (name === "email") {
+            setEmail(value);
+        } else if (name === "message") {
+            setMessage(value);
+        }
+    };
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault(); 
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
 
-    setName('');
-  };
+        setName('');
+        setEmail('');
+        setMessage('');
+    };
 
-  return (
-    <div>
-      <p>
-        Hello {fullName}
-      </p>
-      <form className="form">
-        <input
-          value={fullName}
-          name="fullName"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Full Name"
-        />
-        <button type="button" onClick={handleFormSubmit}>
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+    return (
+        <div>
+            <p>
+                Hello {fullName}
+            </p>
+            <form className="form">
+                <input
+                    value={fullName}
+                    name="fullName"
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="Full Name"
+                />
+                <input
+                    value={email}
+                    name="email"
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="Email"
+                />
+                <textarea
+                    value={message}
+                    name="message"
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="Message"
+                />
+                <button type="button" onClick={handleFormSubmit}>
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
 }
