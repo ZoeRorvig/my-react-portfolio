73e4c794eaed505d65b8449cec761e772408c0d1
Zoe Rorvig <zrorvig@gmail.com>
2023-02-08 00:22:38 -0500

    added second picture and updated footer

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import { SiGithub } from "@react-icons/all-files/si/SiGithub";
+import { SiLinkedin } from "@react-icons/all-files/si/SiLinkedin";
+import { SiInstagram } from "@react-icons/all-files/si/SiInstagram";
 
 export default function Footer() {
     return (
         <footer>
-            <h4>Contact Me!</h4>
             <ul>
                 <li>
-                    <a href='https://www.linkedin.com/in/zoerorvig/' target='_blank' rel='noreferrer' className='footer-links'>LinkedIn</a>
+                    <a href='https://www.linkedin.com/in/zoerorvig/' target='_blank' rel='noreferrer' className='footer-links'><SiLinkedin /></a>
                 </li>
                 <li>
-                    <a href='https://github.com/ZoeRorvig' target='_blank' rel='noreferrer' className='footer-links'>GitHub</a>
+                    <a href='https://github.com/ZoeRorvig' target='_blank' rel='noreferrer' className='footer-links'><SiGithub /></a>
                 </li>
                 <li>
-                    <a href='https://www.instagram.com/mightyzorvig/' target='_blank' rel='noreferrer' className='footer-links'>Instagram</a>
+                    <a href='https://www.instagram.com/mightyzorvig/' target='_blank' rel='noreferrer' className='footer-links'><SiInstagram /></a>
                 </li>
             </ul>
         </footer>
