ac3eeb92bbcd7e5c34eff05ba1e8a5c38c4905fe
Zoe Rorvig <zrorvig@gmail.com>
2023-02-08 00:00:49 -0500

    added icons to resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import resume from '../../images/resume.jpg';
+import { SiJavascript } from "@react-icons/all-files/si/SiJavascript";
+import { FaReact } from "@react-icons/all-files/fa/FaReact";
+import { DiCss3 } from "@react-icons/all-files/di/DiCss3";
+import { RiHtml5Fill } from "@react-icons/all-files/ri/RiHtml5Fill";
+import { GrMysql } from "@react-icons/all-files/gr/GrMysql";
 
 export default function Resume() {
   return (
@@ -11,16 +16,17 @@ export default function Resume() {
         <img className='resume' src={resume} alt='resume' />
       </a>
         <h3>Proficiencies</h3>
-        <ul>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>CSS</li>
-          <li>HTML</li>
-          <li>SQL</li>
+        <ul className='proficienciesList'>
+          <li>JavaScript <SiJavascript /></li>
+          <li>-</li>
+          <li>React <FaReact /></li>
+          <li>-</li>
+          <li>CSS <DiCss3 /></li>
+          <li>-</li>
+          <li>HTML <RiHtml5Fill /></li>
+          <li>-</li>
+          <li>SQL <GrMysql /></li>
         </ul>
     </div>
   );
 }
-
-// TODO: layout
-// TODO: change when hovered over
